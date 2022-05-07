a0484de67013e37194dbf5a3f417dd9031b6ff60
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2022-05-07 20:51:15 -0300

    Add exercises 1, 2, 3 ,4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
-
+const singleRemoveFromArray = function(array, thingToRemove) {
+    let index = array.indexOf(thingToRemove);
+    if (index === -1) {
+        return array;
+    }
+    array.splice(index, 1);
+    return array;
 };
+
+const removeFromArray = function() {
+    let completeArray = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        completeArray = singleRemoveFromArray(completeArray, arguments[i]);
+    }
+    return completeArray;
+}
+
+// let arreglo = ["lkj", "lgkrt", "3", "4"];
+// arregloNuevo = removeFromArray(arreglo, "4", "3");
+// console.log(arregloNuevo);
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
