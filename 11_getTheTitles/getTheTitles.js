228c3c923bc978f7252f9b21d4ae43ffd95fa111
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2022-06-01 16:16:44 -0300

    Adds exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+const getTheTitles = function(arreglo) {
+    const titles = arreglo.map((objetoInterno) => objetoInterno.title);
+    return titles;
 };
 
 // Do not edit below this line
